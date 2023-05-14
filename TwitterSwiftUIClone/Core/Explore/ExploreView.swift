a726c86e2f35d2bbbc3384332b2ee7f93c27615b
Taner Celik <tanercelik2001@gmail.com>
2023-05-14 23:01:04 +0300

    MainTabViews

---
+++ TwitterSwiftUIClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Taner Çelik on 14.05.2023.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Explore View")
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
---
+++ TwitterSwiftUIClone/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Taner Çelik on 14.05.2023.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages View")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
