679d952966a9dc015e61c257dc4c1ae8a00b7803
Taner Celik <tanercelik2001@gmail.com>
2023-05-14 22:04:43 +0300

    Initial Commit

---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Taner Çelik on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
